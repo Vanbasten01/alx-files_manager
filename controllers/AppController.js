b1f790451f41b6438c3a8f1806dc105db949b789
kaleabendrias <kaleabendrias1212@gmail.com>
2024-03-08 16:47:51 +0300

    Add AppController, routes, and server.js files

---
+++ controllers/AppController.js
@@ -0,0 +1,30 @@
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+class AppController {
+  static async getStatus(req, res) {
+    const redisAlive = await redisClient.isAlive();
+    const dbAlive = await dbClient.isAlive();
+    const status = {
+      redis: redisAlive,
+      db: dbAlive,
+    };
+    return res.status(200).json(status);
+  }
+
+  static async getStats(req, res) {
+    try {
+      const nbUsers = await dbClient.nbUsers();
+      const nbFiles = await dbClient.nbFiles();
+      const stats = {
+        users: nbUsers,
+        files: nbFiles,
+      };
+      return res.status(200).json(stats);
+    } catch (error) {
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+}
+
+export default AppController;
